01b36eae579fda8343c6c994cbee3b9d87ee361c
Evan Feng <>
2018-04-28 14:57:50 -0700

    Fix bubble bug on about page

--- js/about.js
+++ js/about.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   setTimeout(function(){
     $(".story_container").removeClass("about_hidden");
-    $("#about_header_area").removeClass("about_header_hidden");
+    // $("#about_header_area").removeClass("about_header_hidden");
   },300);
 });
